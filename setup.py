ab1add1ddfaa0fff484a98a776ef441a55167b59
Envyre-Coding <86875820+Envyre-Development@users.noreply.github.com>
2021-10-16 11:13:02 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,28 +1,33 @@
 from distutils.core import setup
 
+
 with open("README.rst", "r", encoding="utf-8") as f:
     README = f.read()
 
 setup(
   name = 'easy_api_builder',         
   packages = ['easy_api_builder'], 
-  version = '0.1',     
-  license='MIT', 
-  long_description=README,
-  long_description_content_type='text/x-rst',
+  version = '0.2',     
+  license='MIT',     
   package_data={
         'easy_api_builder': ['templates/*']
   },
+  long_description=README,
+  long_description_content_type='text/x-rst',
   include_package_data=True,   
   description = 'easy_api_builder is an easy way to create you own API in python', 
-  install_requires = "Flask",
+  install_requires = ["Flask", "requests"],
   author = 'Areo',  
   author_email = 'areo@envyre.de',      
   url = 'https://github.com/Envyre-Development/easy_api_builder',  
   download_url = 'https://github.com/Envyre-Development/easy_api_builder/archive/refs/tags/easy_api_builder.tar.gz',    
-  keywords = ['api', 'easy', 'api_maker'],   
+  keywords = ['api', 'easy', 'api_maker', "requests", "api_builder"],   
+  project_urls={
+        "Documentation": "https://github.com/Envyre-Development/easy_api_builder",
+        "Issue tracker": "https://github.com/Envyre-Development/easy_api_builder/issues",
+      },
   classifiers=[
-    'Development Status :: 4 - Beta',      
+    'Development Status :: 4 - Beta',     
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
